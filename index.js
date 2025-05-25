97073cc789e989b289ce3beca6a7a808c605768d
Trysses <wj.soares41@gmail.com>
2025-05-25 09:51:48 -0400

    treinando git

---
+++ index.js
@@ -0,0 +1,15 @@
+## crie uma variavel para armazenar o nome e a quantidade de experiencia (xp) de um heroi,
+depois utilize uma estruta de decisão para apresentar algumas das mensagens.a
+
+se o xp for menor que 1000 é = ferro 
+se o xp for entre 1000 e  2000 = ferro 
+se o xp for entre 2000 e 5000 = ferro 
+se o xp for entre 5000 e 7000 = ferro 
+se o xp for entre 7000 e 8000= ferro 
+se o xp for entre 8000 e 9000= ferro 
+se o xp for entre  9000 e 10000= ferro 
+se o xp for maior o igual 10001= ferro
+
+## saida o heroi de nome () está no nivel ()
+
+console.log( "saida o heroi de nome () está no nivel () " )
